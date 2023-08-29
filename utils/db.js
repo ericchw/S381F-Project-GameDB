0e131ad1a41006e11d4591ed450d7eaba81471c9
Eric Chan <48132385+ericchw@users.noreply.github.com>
2023-08-29 15:13:21 +0800

    Change as Mongodb-legacy dependencies
    -Change to mongodb-legacy as callback not support anymore
    -.env for key variable(Please hidden it if PROD)
    -

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,12 @@
-const MongoClient = require("mongodb").MongoClient;
+require('dotenv').config()
+// console.log(process.env)
+
+const MongoClient = require("mongodb-legacy").MongoClient;
 const assert = require('assert');
 
-const mongourl = 'mongodb+srv://gamedb:gamedb@cluster0.z0zhbkh.mongodb.net/';
-const dbName = 'gamedb';
+const mongourl = process.env.MONGOURL; //mongodb+srv://[username]:[password]@cluster0.[dbid].mongodb.net/
+const dbName = process.env.DBNAME;
+const collection = process.env.DBCONNECTION;
 const client = new MongoClient(mongourl);
 
 let _collection;
@@ -11,8 +15,8 @@ let _collection;
 const mongodbConnect = (callback) => {
     client.connect((err) => {
         assert.equal(null, err);
-        let db = client.db(dbName);
-        _collection = db.collection("games")
+        let db = client.db(dbName); //gamedb
+        _collection = db.collection(collection) //games
         callback();
     })
 }
